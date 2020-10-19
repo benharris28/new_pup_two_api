8f43251e3894dbe6ee6e4dbb29a5bfe3d6853f1f
Ben Harris <benjaminharris28@gmail.com>
2020-10-19 09:49:41 -0400

    updated

--- src/product/product-service.js
+++ src/product/product-service.js
@@ -10,7 +10,7 @@ const ProductService = {
         product.attribute = await db.select('*')
             .from('product_attribute')
             .where('product_id', id)
-
+       
 
         return product;
 
@@ -42,7 +42,7 @@ const ProductService = {
         // Iterate over the keys of "OrderedByCategory"
         let products = [];
         for (const category of Object.keys(orderedByCategory)) {
-            //let i = Object.keys(orderedByCategory).indexOf(category);
+            let i = Object.keys(orderedByCategory).indexOf(category);
             
             // 
             let sqlIn = orderedByCategory[category]
@@ -57,7 +57,7 @@ const ProductService = {
                 .whereNull('product.deleted_at')
                 .groupBy('product_attribute.product_id')
                 .first()
-
+          
             console.log(product)
 
             if (typeof product !== "undefined") {
@@ -65,7 +65,63 @@ const ProductService = {
             }
             
         }
+        console.log(products)
         return products;
+        
+    },
+
+    async getProductsForDogs2(db, dog_id) {
+        // Get all attributes and corresponding category_id for that dog_id
+        // Output in an array
+        const dog_attributes = await db
+            .select('attribute_id', 'category_id')
+            .from('dog_attribute')
+            .where('dog_id', dog_id)
+            .whereNull('deleted_at')
+
+        console.log(dog_attributes)
+
+        // Sort function
+        // Takes in an array 
+        let orderedByCategory = {};
+        dog_attributes.forEach(attribute => {
+            if (typeof orderedByCategory['c_' + attribute.category_id] === 'undefined') {
+                orderedByCategory['c_' + attribute.category_id] = [];
+            }
+            orderedByCategory['c_' + attribute.category_id].push(attribute.attribute_id)
+        })
+
+        console.log(orderedByCategory)
+
+        // Iterate over the keys of "OrderedByCategory"
+        let products = [];
+        for (const category of Object.keys(orderedByCategory)) {
+            let i = Object.keys(orderedByCategory).indexOf(category);
+            
+            // 
+            let sqlIn = orderedByCategory[category]
+            let categoryId = Number(category.substring(2));
+
+            // knex query
+            let product = await db('product_attribute')
+                .join('product', 'product.id', '=', 'product_attribute.product_id')
+                .select('product_id')
+                .whereIn('attribute_id', sqlIn)
+                .where('product.category_id', categoryId)
+                .whereNull('product.deleted_at')
+                .groupBy('product_attribute.product_id')
+                .first()
+          
+            console.log(product)
+
+            if (typeof product !== "undefined") {
+                products.push(await this.getById(db, product.product_id))
+            }
+            
+        }
+        console.log(products)
+        return products;
+        
     },
 
     getAllProducts(db, active) {
