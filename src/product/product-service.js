59f662cb40f43711a3ebee846c3d8dd4f1740f59
Ben Harris <benjaminharris28@gmail.com>
2020-10-26 16:19:07 -0400

    added new method

--- src/product/product-service.js
+++ src/product/product-service.js
@@ -70,58 +70,21 @@ const ProductService = {
         
     },
 
-    async getProductsForDogs2(db, dog_id) {
-        // Get all attributes and corresponding category_id for that dog_id
-        // Output in an array
-        const dog_attributes = await db
-            .select('attribute_id', 'category_id')
-            .from('dog_attribute')
-            .where('dog_id', dog_id)
-            .whereNull('deleted_at')
-
-        console.log(dog_attributes)
-
-        // Sort function
-        // Takes in an array 
-        let orderedByCategory = {};
-        dog_attributes.forEach(attribute => {
-            if (typeof orderedByCategory['c_' + attribute.category_id] === 'undefined') {
-                orderedByCategory['c_' + attribute.category_id] = [];
-            }
-            orderedByCategory['c_' + attribute.category_id].push(attribute.attribute_id)
-        })
-
-        console.log(orderedByCategory)
-
-        // Iterate over the keys of "OrderedByCategory"
-        let products = [];
-        for (const category of Object.keys(orderedByCategory)) {
-            let i = Object.keys(orderedByCategory).indexOf(category);
-            
-            // 
-            let sqlIn = orderedByCategory[category]
-            let categoryId = Number(category.substring(2));
+    async getProductsForDogs2(db) {
+            let dogAttribute = [2,12]
 
             // knex query
             let product = await db('product_attribute')
                 .join('product', 'product.id', '=', 'product_attribute.product_id')
                 .select('product_id')
-                .whereIn('attribute_id', sqlIn)
-                .where('product.category_id', categoryId)
+                .whereIn('attribute_id', dogAttribute)
+                .where('product.category_id', 1)
                 .whereNull('product.deleted_at')
                 .groupBy('product_attribute.product_id')
                 .first()
           
             console.log(product)
 
-            if (typeof product !== "undefined") {
-                products.push(await this.getById(db, product.product_id))
-            }
-            
-        }
-        console.log(products)
-        return products;
-        
     },
 
     getAllProducts(db, active) {
